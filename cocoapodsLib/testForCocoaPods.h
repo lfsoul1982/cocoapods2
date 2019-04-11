de77201ae6acead360cca6d7401433b5eae15269
lifei.lf <lifei.lf@alibaba-inc.com>
2019-04-11 15:06:23 +0800

    cocoapodsLib

---
+++ cocoapodsLib/testForCocoaPods.h
@@ -0,0 +1,17 @@
+//
+//  testForCocoaPods.h
+//  cocoapodsTest1
+//
+//  Created by lifei on 2019/4/10.
+//  Copyright © 2019 lifei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface testForCocoaPods : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cocoapodsLib/testForCocoaPods.m
@@ -0,0 +1,13 @@
+//
+//  testForCocoaPods.m
+//  cocoapodsTest1
+//
+//  Created by lifei on 2019/4/10.
+//  Copyright © 2019 lifei. All rights reserved.
+//
+
+#import "testForCocoaPods.h"
+
+@implementation testForCocoaPods
+
+@end
